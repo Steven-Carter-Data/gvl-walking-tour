c8bf8bb8d653958bdc5625a8b40193097f46bd4a
Steven Carter <sdcarter22@gmail.com>
2025-09-12 20:03:59 -0400

    Add Apple Pay support after domain verification

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
 
     // Create Stripe checkout session
     const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
+      payment_method_types: ['card', 'apple_pay'],
       line_items: [
         {
           price_data: {
